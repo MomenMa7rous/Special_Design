b9a48a8d97be03291cedbb3ba59c5716add4e5e2
momenma7rous <momenma7rous@gmail.com>
2023-06-22 15:53:45 +0300

    Create About Section.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// Header Scroll 
+const header = document.querySelector("header")
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 20) {
+    header.style.cssText = '--color: #000;'
+    header.style.background = '#fff'
+  } else {
+    header.style.cssText = '--color: #fff;'
+    header.style.background = 'none'
+  }
+})
+
 // Header Menu Button
 const menuBtn = document.getElementById("menuBtn")
 const menu = document.querySelector("header ul.mobile-nav")
